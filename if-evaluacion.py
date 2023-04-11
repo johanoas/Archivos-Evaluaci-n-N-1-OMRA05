ad2b304368a4211050676a76e2e9c5605ce51c71
johanoas <johan.barriga@inacapmail.cl>
2023-04-11 00:59:28 +0000

    Add DEVASC.txt file to devasc-study-team

---
+++ if-evaluacion.py
@@ -0,0 +1,4 @@
+Nombre = input("Cual es tu nombre? ")
+Seccion = input("Cual es tu Sección? ")
+CD = input( "Cual es tu sede? ")
+print(" Hola " + Nombre + " de la sede " + CD + " sección " + Seccion)
